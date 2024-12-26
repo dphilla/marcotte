5a71991b5bf4d04951a9a32eabc31871e0a410a7
Daniel Phillips <daniel@loopholelabs.io>
2024-12-25 18:38:38 -0600

    Add dirent wrappers

---
+++ builder/src/dirent/opendir.c
@@ -0,0 +1,24 @@
+#include <dirent.h>
+#include <fcntl.h>      /* O_RDONLY, O_DIRECTORY  */
+#include <stdlib.h>     /* malloc() */
+#include <string.h>     /* memset() */
+#include <unistd.h>     /* open() */
+
+DIR *opendir(const char *name)
+{
+    /* TODO - double check this : Typically open() with O_DIRECTORY. */
+    int fd = open(name, O_RDONLY, 0);
+    if (fd < 0) return NULL;
+
+    DIR *dirp = (DIR*)malloc(sizeof(DIR));
+    if (!dirp) {
+        close(fd);
+        return NULL;
+    }
+    dirp->fd = fd;
+    dirp->buf_pos = 0;
+    dirp->buf_end = 0;
+    memset(dirp->buffer, 0, sizeof(dirp->buffer));
+    return dirp;
+}
+
